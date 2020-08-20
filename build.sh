0388bd0548ccc58dd142d8e42448043b3d52069d
神楽坂·喵 <7751303+KagurazakaNyaa@users.noreply.github.com>
2020-08-20 22:59:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-git reset --hard HEAD
-git clean -f -d
-git pull
 version=$(curl https://raw.githubusercontent.com/screeps/screeps/master/package.json | jq -r .version)
 currentversion=$(cat currentversion)
 echo $version > currentversion
@@ -12,13 +9,13 @@ if [[ "$1" == "ptr" ]]; then
 fi
 sed -i 's/^ENV SCREEPS_VERSION.*$/ENV SCREEPS_VERSION '$version'/i' Dockerfile
 date > timestamp
+git config user.name github-actions
+git config user.email github-actions@github.com
 git add timestamp
 git add currentversion
-git config --global user.email i@kagurazakanyaa.com
-git config --global user.name KagurazakaNyaa
-git commit --no-gpg-sign -a -m "Auto Update to screeps "$version
+git commit -a -m "Auto Update to screeps "$version
 git tag -f $version
 if [[ "$version" != "ptr" ]]; then
 	git tag -f latest
 fi
-git push origin master --tags --force
+git push
